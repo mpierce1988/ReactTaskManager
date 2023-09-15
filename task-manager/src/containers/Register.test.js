ea0a364e23b95f0a266d2021f52aee43d8e0910c
michaelpierce <mpierce1138@gmail.com>
2023-09-15 20:07:35 -0300

    Implemented Register.test.js

--- task-manager/src/containers/Register.test.js
+++ task-manager/src/containers/Register.test.js
@@ -2,76 +2,85 @@ import { render, fireEvent } from '@testing-library/react';
 import { Register } from './Register';
 
 describe('Register Component', () => {
-    // test that email textbox is rendered
-test('email textbox is rendered', () => {
-    const { getByLabelText } = render(<Register />);
-    const emailTextbox = getByLabelText(/email/i);
-    expect(emailTextbox).toBeInTheDocument();
-});
+        // test that email textbox is rendered
+    test('email textbox is rendered', () => {
+        const { getByLabelText } = render(<Register />);
+        const emailTextbox = getByLabelText(/email/i);
+        expect(emailTextbox).toBeInTheDocument();
+    });
 
-// test that password textbox is rendered
-test('password textbox is rendered', () => {
-    const { getByLabelText } = render(<Register />);
-    const passwordTextBox = getByLabelText(/password/i);
-    expect(passwordTextBox).toBeInTheDocument();
-});
+    // test that password textbox is rendered
+    test('password textbox is rendered', () => {
+        const { getByLabelText } = render(<Register />);
+        const passwordTextBox = getByLabelText(/^password$/i);
+        expect(passwordTextBox).toBeInTheDocument();
+    });
 
- // test that name textbox is rendered
- test('name textbox is rendered', () => {
-    const { getByLabelText } = render(<Register />);
-    const nameTextBox = getByLabelText(/name/i);
-    expect(nameTextBox).toBeInTheDocument();
-});
+    // test that name textbox is rendered
+    test('name textbox is rendered', () => {
+        const { getByLabelText } = render(<Register />);
+        const nameTextBox = getByLabelText(/name/i);
+        expect(nameTextBox).toBeInTheDocument();
+    });
 
-// test that login button is rendered
-test('login button is rendered', () => {
-    const { getByText } = render(<Register />);
-    const loginButton = getByText(/register/i);
-    expect(loginButton).toBeInTheDocument();
-    expect(loginButton.tagName).toBe('BUTTON');
-});
+    // test that login button is rendered
+    test('register button is rendered', () => {
+        const { getByRole } = render(<Register />);
+        const loginButton = getByRole('button', { name: /register/i});
+        expect(loginButton).toBeInTheDocument();
+        expect(loginButton.tagName).toBe('BUTTON');
+    });
 
-// test that email is required
-test('email is required', async () => {
-    const { getByText, getByLabelText, findByText } = render(<Register />);
-    const loginButton = getByText(/register/i);
-    const passwordTextBox = getByLabelText(/password/i);
-    
-    fireEvent.change(passwordTextBox, { target: { value: 'Password123!' } });
-    fireEvent.click(loginButton);
-    
-    const emailRequired = await findByText(/email is required/i);
-    
-    expect(emailRequired).toBeInTheDocument();    
-});
+    // test that email is required
+    test('email is required', async () => {
+        const { getByRole, getByLabelText, findByText, queryByText } = render(<Register />);
+        const loginButton = getByRole('button', { name: /register/i });
+        const passwordTextBox = getByLabelText(/^password$/i);
+        const nameTextBox = getByLabelText(/name/i);
+        
+        fireEvent.change(passwordTextBox, { target: { value: 'Password123!' } });
+        fireEvent.change(nameTextBox, { target: { value: 'Michael' } });
+        fireEvent.click(loginButton);        
+       
+        expect(await findByText(/email is required/i)).toBeInTheDocument();
+        //test password and name error messages are NOT displayed
+        expect(queryByText(/password is required/i)).toBeNull();
+        expect(queryByText(/name is required/i)).toBeNull();   
+    });
 
-// test that email is required
-test('password is required', async () => {
-    const { getByText, getByLabelText, findByText } = render(<Register />);
-    const loginButton = getByText(/register/i);
-    const emailTextBox = getByLabelText(/email/i);
-    
-    emailTextBox.textContent = "test@gmail.com";
-    fireEvent.click(loginButton);
-    
-    
-    const passwordRequired = await findByText(/password is required/i);
-    expect(passwordRequired).toBeInTheDocument();    
-});
+    // test that email is required
+    test('password is required', async () => {
+        const { getByRole, getByLabelText, findByText, queryByText } = render(<Register />);
+        const loginButton = getByRole('button', { name: /register/i });
+        const emailTextBox = getByLabelText(/email/i);
+        const nameTextBox = getByLabelText(/name/i);
+        
+        fireEvent.change(emailTextBox, { target: { value: 'test@gmail.com'}});
+        fireEvent.change(nameTextBox, { target: { value: 'Michael'}});        
+        fireEvent.click(loginButton);
 
- // test that name is required
- test('name is required', async () => {
-    const { getByRole, getByLabelText, findByText } = render(<Register />);
-    const loginButton = getByRole("button", { name: /login/i });
-    const emailTextBox = getByLabelText(/email/i);
-    const passwordTextBox = getByLabelText(/password/i);
-    
-    fireEvent.change(emailTextBox, {target: { value: "Test@gmail.com"}});
-    fireEvent.change(passwordTextBox, { target: { value: 'Password123!' } });
-    fireEvent.click(loginButton);    
-    
-    const nameRequired = await findByText(/name is required/i);
-    expect(nameRequired).toBeInTheDocument();    
-});
+        expect(await findByText(/password is required/i)).toBeInTheDocument();
+        //test email and name error messages are NOT displayed
+        expect(queryByText(/email is required/i)).toBeNull();
+        expect(queryByText(/name is required/i)).toBeNull();  
+    });
+
+    // test that name is required
+    test('name is required', async () => {
+        const { getByRole, getByLabelText, findByText, queryByText } = render(<Register />);
+        const loginButton = getByRole("button", { name: /register/i });
+        const emailTextBox = getByLabelText(/email/i);
+        const passwordTextBox = getByLabelText(/^password$/i);
+        
+        fireEvent.change(emailTextBox, {target: { value: "Test@gmail.com"}});
+        fireEvent.change(passwordTextBox, { target: { value: 'Password123!' } });
+        fireEvent.click(loginButton);    
+        
+        
+        expect(await findByText(/name is required/i)).toBeInTheDocument();
+        //test email and password error messages are NOT displayed
+        expect(queryByText(/email is required/i)).toBeNull();
+        expect(queryByText(/password is required/i)).toBeNull();   
+    });
 })
 
