e012a1e41d652293b7d5efcc27e14ca0480a1152
michaelpierce <mpierce1138@gmail.com>
2023-09-15 18:48:50 -0300

    Added Logout button to header, header links now change is user is logged in/out

--- task-manager/src/components/Header.js
+++ task-manager/src/components/Header.js
@@ -1,14 +1,32 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useUser } from '../contexts/UserContext';
 
 export function Header() {
+    const navigate  = useNavigate();
+    const { userId, setUserId } = useUser();
+
+    const handleLogout = () => {
+        setUserId(null);
+        navigate('/login');
+    }
+
     return (
         <>
             <nav>
                 <Link to="/">Home</Link> | 
-                | <Link to="/register">Register</Link> | 
-                | <Link to="/login">Login</Link> | 
-                | <Link to="/tasks">Tasks</Link> | 
-                | <Link to="/about">About</Link>
+                { userId === null ? (
+                    <>
+                        | <Link to="/register">Register</Link> | 
+                        | <Link to="/login">Login</Link>  
+                    </>
+                ) : (
+                    <>
+                        | <Link to="/tasks">Tasks</Link> | 
+                        | <Link to="/about">About</Link> |
+                        | <button onClick={handleLogout}>Logout</button>
+                    </>
+                ) }                
+                
             </nav>
         </>
     );
