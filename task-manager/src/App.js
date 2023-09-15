1eadb2042f48b9dec939c0eb8e1599dd6093ec2c
michaelpierce <mpierce1138@gmail.com>
2023-09-15 18:21:58 -0300

    Updated App.js and Index.js to use UserProvider

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import { Home } from './containers/Home';
+import { Header } from './components/Header';
+import { Login } from './containers/Login';
+import { Register } from './containers/Register';
+import { TaskList } from './containers/TaskList';
+import { About } from './containers/About';
+import { Routes, Route } from 'react-router-dom';
+import { UserProvider } from './contexts/UserContext';
 
 function App() {
   return (
-    <Home />
+    <div className="App">
+      <UserProvider>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/tasks" element={<TaskList />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </UserProvider>
+    </div>
   );
 }
 
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -4,23 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Header } from './components/Header';
-import { Login } from './containers/Login';
-import { Register } from './containers/Register';
-import { TaskList } from './containers/TaskList';
-import { About } from './containers/About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="tasks" element={<TaskList />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <App />
   </BrowserRouter>
 );
 
