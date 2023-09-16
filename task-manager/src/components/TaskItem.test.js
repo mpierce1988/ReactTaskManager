abc66a3cbe39d12958fbe340f0deeada3005b5bf
michaelpierce <mpierce1138@gmail.com>
2023-09-16 19:09:36 -0300

    Updated tests for TaskItem, TaskItem is now a non-dynamic component

--- task-manager/src/components/TaskItem.test.js
+++ task-manager/src/components/TaskItem.test.js
@@ -1,18 +1,13 @@
 import { TaskItem } from './TaskItem';
 import { render, screen } from '@testing-library/react';
-import * as apiFunctions from '../functions/apiFunctions';
-
-// Mock the getTask function
-apiFunctions.getTask = jest.fn();
 
 describe('TaskItem Component', () => {
     // Test that the task name is rendered
     test('task name is rendered', async () => {
         const taskName = 'Task 1';
-        // Mock the response
-        apiFunctions.getTask.mockResolvedValueOnce({status: "Success", task: { id: 1, userId: 1, name: taskName, description: 'Description 1'}});
+        const taskDescription = "Description 1";        
 
-        const { findByText } = render(<TaskItem taskId={1} />);
+        const { findByText } = render(<TaskItem name={taskName} description={taskDescription} id={1}/>);
 
         const taskNameElement = await findByText(taskName);
         expect(taskNameElement).toBeInTheDocument();
@@ -20,11 +15,10 @@ describe('TaskItem Component', () => {
 
     // Test that task description is rendered
     test('task description is rendered', async () => {
+        const taskName = 'Task 1';
         const taskDescription = 'Description 1';
-        // Mock the response
-        apiFunctions.getTask.mockResolvedValueOnce({status: "Success", task: { id: 1, userId: 1, name: 'Task 1', description: taskDescription}});
 
-        const { findByText } = render(<TaskItem taskId={1} />);
+        const { findByText } = render(<TaskItem name={taskName} description={taskDescription} id={1} />);
 
         const taskDescriptionElement = await findByText(taskDescription);
 
@@ -35,12 +29,11 @@ describe('TaskItem Component', () => {
     test('edit task button is rendered', async () => {
         const taskName = 'Task 1';
         const taskDescription = 'Description 1';
-        // Mock the response
-        apiFunctions.getTask.mockResolvedValueOnce({status: "Success", task: { id: 1, userId: 1, name: taskName, description: taskDescription}});
+        
 
-        const { findByRole } = render(<TaskItem taskId={1} />);
+        const { findByRole } = render(<TaskItem name={taskName} description={taskDescription} id={1} />);
 
-        const editTaskButton = findByRole('button', { name: /edit/i});
+        const editTaskButton = await findByRole('button', { name: /edit/i});
 
         expect(editTaskButton).toBeInTheDocument();
     });
@@ -48,14 +41,13 @@ describe('TaskItem Component', () => {
     // test that the delete task button is rendered
     test('delete task button is rendered', async () => {
         const taskName = 'Task 1';
-        const taskDescription = 'Description 1';
-        // Mock the response
-        apiFunctions.getTask.mockResolvedValueOnce({status: "Success", task: { id: 1, userId: 1, name: taskName, description: taskDescription}});
+        const taskDescription = 'Description 1';        
 
-        const { findByRole } = render(<TaskItem taskId={1} />);
+        const { findByRole } = render(<TaskItem name={taskName} description={taskDescription} id={1} />);
 
-        const deleteTaskButton = findByRole('button', { name: /delete/i});
+        const deleteTaskButton = await findByRole('button', { name: /delete/i});
 
         expect(deleteTaskButton).toBeInTheDocument();
     });
+   
 });
