b3324ead4f62f3fe9b0f2d1196a9144c4ceebccf
michaelpierce <mpierce1138@gmail.com>
2023-09-16 19:09:45 -0300

    Added buttons to TaskItem

--- task-manager/src/components/TaskItem.js
+++ task-manager/src/components/TaskItem.js
@@ -1,8 +1,12 @@
-export function TaskItem({name, description}) {
+
+
+export function TaskItem({id, name, description}) {
     return (
         <>
             <h2>{name}</h2>
             <p>{description}</p>
+            <button role='button' aria-label='edit' name='edit'>Edit</button>
+            <button role='button' aria-label='delete' name='delete'>Delete</button>
         </>
     );
 }
