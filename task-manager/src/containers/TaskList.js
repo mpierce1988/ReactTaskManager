f5518cc8b49b469e8903dd042632bff6c95ec5a2
michaelpierce <mpierce1138@gmail.com>
2023-09-18 13:52:52 -0300

    Created TaskList skeleton

--- task-manager/src/containers/TaskList.js
+++ task-manager/src/containers/TaskList.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { TaskItem } from "../components/TaskItem";
 import { getTasks } from "../functions/apiFunctions";
 import { useUser } from "../contexts/UserContext";
+import { Link } from "react-router-dom";
 
 export function TaskList() {
     const {userId} = useUser();
@@ -11,21 +12,27 @@ export function TaskList() {
     
     useEffect(() => {
         const fetchData = async () => {
-            if(!userId) return;
+            try {
+                if(!userId) return;
 
-        setLoading(true);
-        setError('');
+                setLoading(true);
+                setError('');
 
-        const data = await getTasks(userId);
+                const data = await getTasks(userId);
 
-        // Check for errors
-        if(data.status != 'Success') {
-            setError('Unable to retrieve tasks');
-        } else {
-            setTasks(data.tasks);
-        }
-
-        setLoading(false);
+                // Check for errors
+                if(data.status != 'Success') {
+                    setError('Unable to retrieve tasks');
+                    
+                } else {
+                    setTasks(data.tasks);
+                    
+                }
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }            
         }
 
         fetchData();
@@ -38,9 +45,9 @@ export function TaskList() {
             {!userId && <p>User not logged in</p>}
             {loading && <p>Loading...</p>}
             {error && <p>{error}</p>}
-            {tasks.length == 0 && <p>No tasks found</p>}
-            {userId && <button role='button' name='create'>Create Task</button>}
-            {tasks.length > 0 && 
+            {tasks && tasks.length == 0 && <p>No tasks found</p>}
+            {userId && <Link to="/tasks/create" role="button">Create Task</Link>}
+            {tasks && tasks.length > 0 && 
                 <ul>
                     {tasks.map(t => <li key={t.id}><TaskItem key={t.id} name={t.name} description={t.description} /></li>)}
                 </ul> 
