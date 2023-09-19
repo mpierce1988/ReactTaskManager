15a8b00956d0f777830d0cc32d575b5be7043f92
michaelpierce <mpierce1138@gmail.com>
2023-09-19 19:58:05 -0300

    Implemented Edit button that navigates to Edit Task page

--- task-manager/src/components/TaskItem.js
+++ task-manager/src/components/TaskItem.js
@@ -1,11 +1,17 @@
+import { useNavigate } from "react-router-dom";
 
+export function TaskItem({ userId, taskId, name, description,}) {
+    const navigate = useNavigate();
 
-export function TaskItem({ name, description}) {
+    const goToUpdateTask = () => {
+        navigate('/tasks/' + taskId);
+    }
+
     return (
         <>
             <h2>{name}</h2>
             <p>{description}</p>
-            <button role='button' aria-label='edit' name='edit'>Edit</button>
+            <button role='button' aria-label='edit' name='edit' onClick={goToUpdateTask}>Edit</button>
             <button role='button' aria-label='delete' name='delete'>Delete</button>
         </>
     );
