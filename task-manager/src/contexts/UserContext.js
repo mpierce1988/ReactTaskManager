c986da8be8304eab6119e99babb621c660c99ece
michaelpierce <mpierce1138@gmail.com>
2023-09-19 20:06:07 -0300

    Added localStore to userContext to userId is persisted across page reloads

--- task-manager/src/contexts/UserContext.js
+++ task-manager/src/contexts/UserContext.js
@@ -1,4 +1,4 @@
-import {createContext, useContext, useState } from 'react';
+import {createContext, useContext, useState, useEffect } from 'react';
 
 const UserContext = createContext();
 
@@ -9,9 +9,28 @@ export const useUser = () => {
 export const UserProvider = ({ children }) => {
     const [userId, setUserId] = useState(null);
 
+    // load initial userId from localStorage if available
+    useEffect(() => {
+        const storageUserId = localStorage.getItem('userId');
+        if(storageUserId) {
+            setUserId(storageUserId);
+        }
+    });
+
+    const setAndStoreUserId = (newUserId) => {
+        setUserId(newUserId);
+
+        // if userId is not null, save to local storage. otherwise, remove userId from local storage
+        if(newUserId) {
+            localStorage.setItem('userId', newUserId);
+        } else {
+            localStorage.removeItem('userId');
+        }
+    }
+
     const value = {
         userId,
-        setUserId
+        setUserId: setAndStoreUserId
     };
 
     return (
