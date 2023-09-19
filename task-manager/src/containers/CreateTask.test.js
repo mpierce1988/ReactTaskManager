71bc21959bf882822a372a6cb32e417abd71ffcd
michaelpierce <mpierce1138@gmail.com>
2023-09-19 17:52:57 -0300

    Fixed issues with CreateTask tests

--- task-manager/src/containers/CreateTask.test.js
+++ task-manager/src/containers/CreateTask.test.js
@@ -1,5 +1,5 @@
 import { CreateTask } from "./CreateTask";
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent, screen, act } from "@testing-library/react";
 import * as apiFunctions from "../functions/apiFunctions";
 import { useUser } from "../contexts/UserContext";
 
@@ -11,62 +11,73 @@ jest.mock("../contexts/UserContext", () => ({
     useUser: jest.fn(),
 }));
 
-// Set a default mock userUser with userId 1
-useUser.mockReturnValue({userId: 1, setUserId: jest.fn()});
+beforeEach(() => {
+    useUser.mockReturnValue({userId: 1, setUserId: jest.fn()});
+});
 
 describe('CreateTask Component', () => {
     // test that name textbox is rendered
     test('name textbox is rendered', async () => {
-        const { getByLabelText } = render(<CreateTask />);
-        const nameTextBox = getByLabelText(/name/i);
+        render(<CreateTask />);
+
+        const nameTextBox = screen.getByLabelText(/name/i);
         expect(nameTextBox).toBeInTheDocument();
     });
 
     // test that description textbox is rendered
     test('description textbox is rendered', async () => {
-        const { getByLabelText } = render(<CreateTask />);
-        const descriptionTextBox = getByLabelText(/description/i);
+        render(<CreateTask />);
+
+        const descriptionTextBox = screen.getByLabelText(/description/i);
         expect(descriptionTextBox).toBeInTheDocument();
     });
 
     // test that create task button is rendered
-    test('create task button is rendered', () => {
-        const { getByRole } = render(<CreateTask />);
-        const createTaskButton = getByRole('button', { name: /create/i });
+    test('create task button is rendered', async () => {
+        render(<CreateTask />);
+
+        const createTaskButton = screen.getByRole('button', { name: /create/i });
         expect(createTaskButton).toBeInTheDocument();
     });
 
     // test that clear button is rendered
-    test('clear button is rendered', () => {
-        const { getByRole } = render(<CreateTask />);
-        const clearTaskButton = getByRole('button', { name: /clear/i });
+    test('reset button is rendered', async () => {
+        render(<CreateTask />);
+
+        const clearTaskButton = screen.getByRole('button', { name: /reset/i });
         expect(clearTaskButton).toBeInTheDocument();
     });
 
     // test that name is required
     test('name is required displays error message', async () => {
-        const { getByRole, getByLabelText, findByText } = render(<CreateTask />);
-        const createTaskButton = getByRole('button', { name: /create/i });
-        const descriptionTextBox = getByLabelText(/description/i);
+        render(<CreateTask />);
 
-        fireEvent.change(descriptionTextBox, { target: { value: 'Description 1' } });
-        fireEvent.click(createTaskButton);
+        const createTaskButton = screen.getByRole('button', { name: /create/i });
+        const descriptionTextBox = screen.getByLabelText(/description/i);
 
-        const nameRequired = await findByText(/name is required/i);
+        await act(async () => {
+            fireEvent.change(descriptionTextBox, { target: { value: 'Description 1' } });
+            fireEvent.click(createTaskButton);
+        });
+
+        const nameRequired = await screen.findByText(/name is required/i);
 
         expect(nameRequired).toBeInTheDocument();
     });
 
     // test that description is required
     test('description is required displays error message', async () => {
-        const { getByRole, getByLabelText, findByText } = render(<CreateTask />);
-        const createTaskButton = getByRole('button', { name: /create/i });
-        const nameTextBox = getByLabelText(/name/i);
+        render(<CreateTask />);
 
-        fireEvent.change(nameTextBox, { target: { value: 'Task 1' } });
-        fireEvent.click(createTaskButton);
+        const createTaskButton = screen.getByRole('button', { name: /create/i });
+        const nameTextBox = screen.getByLabelText(/name/i);
 
-        const descriptionRequired = await findByText(/description is required/i);
+        await act(async () => {
+            fireEvent.change(nameTextBox, { target: { value: 'Task 1' } });
+            fireEvent.click(createTaskButton);
+        });
+
+        const descriptionRequired = await screen.queryByText(/description is required/i);
 
         expect(descriptionRequired).toBeInTheDocument();
     });
@@ -74,21 +85,23 @@ describe('CreateTask Component', () => {
     // test that a valid task is successfully created, shows success message
     test('valid task is successfully created, shows success message', async () => {
         // mock the createTask function
-        apiFunctions.createTask.mockResolvedValueOnce({status: "Success", task: {id: 2, userId: 1, name: "Task 1", description: "Description 1"}});
+        apiFunctions.createTask.mockResolvedValueOnce({ status: "Success", task: { id: 2, userId: 1, name: "Task 1", description: "Description 1" } });
         // mock the user with user id 1
-        useUser.mockReturnValue({userId: 1, setUserId: jest.fn()});
+        useUser.mockReturnValue({ userId: 1, setUserId: jest.fn() });
 
+        render(<CreateTask />);
 
-        const { getByRole, getByLabelText, findByText } = render(<CreateTask />);
-        const createTaskButton = getByRole('button', { name: /create/i });
-        const nameTextBox = getByLabelText(/name/i);
-        const descriptionTextBox = getByLabelText(/description/i);
+        const createTaskButton = screen.getByRole('button', { name: /create/i });
+        const nameTextBox = screen.getByLabelText(/name/i);
+        const descriptionTextBox = screen.getByLabelText(/description/i);
 
-        fireEvent.change(nameTextBox, { target: { value: 'Task 1' } });
-        fireEvent.change(descriptionTextBox, { target: { value: 'Description 1' } });
-        fireEvent.click(createTaskButton);
+        await act(async () => {
+            fireEvent.change(nameTextBox, { target: { value: 'Task 1' } });
+            fireEvent.change(descriptionTextBox, { target: { value: 'Description 1' } });
+            fireEvent.click(createTaskButton);
+        });
 
-        const taskCreated = await findByText(/task created/i);
+        const taskCreated = await screen.findByText(/task created/i);
 
         expect(taskCreated).toBeInTheDocument();
     });
@@ -96,19 +109,22 @@ describe('CreateTask Component', () => {
     // test that a valid task creation clears the form
     test('valid task creation clears form', async () => {
         // mock the createTask function
-        apiFunctions.createTask.mockResolvedValueOnce({status: "Success", task: {id: 2, userId: 1, name: "Task 1", description: "Description 1"}});
+        apiFunctions.createTask.mockResolvedValueOnce({ status: "Success", task: { id: 2, userId: 1, name: "Task 1", description: "Description 1" } });
         // mock the user with user id 1
-        useUser.mockReturnValue({userId: 1, setUserId: jest.fn()});
+        useUser.mockReturnValue({ userId: 1, setUserId: jest.fn() });
 
-        const { getByRole, getByLabelText, findByText } = render(<CreateTask />);
-        const createTaskButton = getByRole('button', { name: /create/i });
-        const nameTextBox = getByLabelText(/name/i);
-        const descriptionTextBox = getByLabelText(/description/i);
+        render(<CreateTask />);
 
-        fireEvent.change(nameTextBox, { target: { value: 'Task 1' } });
-        fireEvent.change(descriptionTextBox, { target: { value: 'Description 1' } });
-        fireEvent.click(createTaskButton); 
-        
+        const createTaskButton = screen.getByRole('button', { name: /create/i });
+        const nameTextBox = screen.getByLabelText(/name/i);
+        const descriptionTextBox = screen.getByLabelText(/description/i);
+
+        await act(async () => {
+            fireEvent.change(nameTextBox, { target: { value: 'Task 1' } });
+            fireEvent.change(descriptionTextBox, { target: { value: 'Description 1' } });
+            fireEvent.click(createTaskButton);
+        });
+
         expect(nameTextBox.value).toBe('');
         expect(descriptionTextBox.value).toBe('');
     });
@@ -116,20 +132,23 @@ describe('CreateTask Component', () => {
     // test that unsuccessful save displays error message
     test('unsuccessful save displays error message', async () => {
         // mock an error from the server
-        apiFunctions.createTask.mockResolvedValueOnce({status: "Error", message: "Error creating task"});
+        apiFunctions.createTask.mockResolvedValueOnce({ status: "Error", message: "Error creating task" });
         // mock the user with user id 1
         useUser.mockReturnValue({ userId: 1, setUserId: jest.fn() });
 
-        const { getByRole, getByLabelText, findByText } = render(<CreateTask />);
-        const createTaskButton = getByRole('button', { name: /create/i });
-        const nameTextBox = getByLabelText(/name/i);
-        const descriptionTextBox = getByLabelText(/description/i);
+        render(<CreateTask />);
 
-        fireEvent.change(nameTextBox, { target: { value: 'Task 1' } });
-        fireEvent.change(descriptionTextBox, { target: { value: 'Description 1' } });
-        fireEvent.click(createTaskButton); 
-        
-        const errorMessage = await findByText(/error creating task/i);
+        const createTaskButton = screen.getByRole('button', { name: /create/i });
+        const nameTextBox = screen.getByLabelText(/name/i);
+        const descriptionTextBox = screen.getByLabelText(/description/i);
+
+        await act(async () => {
+            fireEvent.change(nameTextBox, { target: { value: 'Task 1' } });
+            fireEvent.change(descriptionTextBox, { target: { value: 'Description 1' } });
+            fireEvent.click(createTaskButton);
+        });
+
+        const errorMessage = await screen.findByText(/error creating task/i);
         expect(errorMessage).toBeInTheDocument();
     });
 
@@ -138,8 +157,9 @@ describe('CreateTask Component', () => {
         // mock a non-logged in user
         useUser.mockReturnValue({ userId: null, setUserId: jest.fn() });
 
-        const { findByText } = render(<CreateTask />);
-        const errorMessage = await findByText(/not logged in/i);
+        render(<CreateTask />);
+
+        const errorMessage = await screen.findByText(/not logged in/i);
         expect(errorMessage).toBeInTheDocument();
     });
 });
