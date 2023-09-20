1798a74c367333d45c1babee462b9cc444c213fb
michaelpierce <mpierce1138@gmail.com>
2023-09-20 12:54:59 -0300

    Wrap main content inside a container

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -10,23 +10,26 @@ import { UserProvider } from './contexts/UserContext';
 import { CreateTask } from './containers/CreateTask';
 import { UpdateTask } from './containers/UpdateTask';
 import { DeleteTask } from './containers/DeleteTask';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <UserProvider>
         <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/tasks" element={<TaskList />} />
-            <Route path="/tasks/create" element={<CreateTask />} />
-            <Route path="/tasks/:taskId" element={<UpdateTask />} />
-            <Route path="/tasks/:taskId/delete" element={<DeleteTask />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </UserProvider>
+        <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/tasks" element={<TaskList />} />
+          <Route path="/tasks/create" element={<CreateTask />} />
+          <Route path="/tasks/:taskId" element={<UpdateTask />} />
+          <Route path="/tasks/:taskId/delete" element={<DeleteTask />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        </Container>
+      </UserProvider>
     </div>
   );
 }
