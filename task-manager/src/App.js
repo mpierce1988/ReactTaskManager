f3edcb8a7aeafda87b81ada896ae9bc876b2a3ca
michaelpierce <mpierce1138@gmail.com>
2023-09-19 22:08:55 -0300

    Added routing to deleteTask page

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -9,6 +9,7 @@ import { Routes, Route } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
 import { CreateTask } from './containers/CreateTask';
 import { UpdateTask } from './containers/UpdateTask';
+import { DeleteTask } from './containers/DeleteTask';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
             <Route path="/tasks" element={<TaskList />} />
             <Route path="/tasks/create" element={<CreateTask />} />
             <Route path="/tasks/:taskId" element={<UpdateTask />} />
+            <Route path="/tasks/:taskId/delete" element={<DeleteTask />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </UserProvider>
