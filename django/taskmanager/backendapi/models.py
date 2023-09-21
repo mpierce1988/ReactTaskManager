057cdcabad80dc9a14c22111c740927cbcd37557
michaelpierce <mpierce1138@gmail.com>
2023-09-21 17:47:30 -0300

    Updated models to have id

--- django/taskmanager/backendapi/models.py
+++ django/taskmanager/backendapi/models.py
@@ -3,11 +3,13 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
 
 class Task(models.Model):
+    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField()
