d2e1777c1a3e7ea200f0e42677c91fd88f33bd55
michaelpierce <mpierce1138@gmail.com>
2023-09-21 14:20:04 -0300

    Added bootstrap styling to Login

--- task-manager/src/containers/Login.js
+++ task-manager/src/containers/Login.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useUser } from '../contexts/UserContext';
 import { useNavigate } from 'react-router-dom';
 import { login } from '../functions/apiFunctions';
+import { Form, Button, Alert } from 'react-bootstrap';
 
 export function Login(){
     const { setUserId } = useUser();
@@ -39,17 +40,21 @@ export function Login(){
         setPasswordError('');
 
         try {
+            let errorCount = 0;
+
             // validate email was entered
             if (!email || email === '') {
-                setEmailError('Email is required');                
+                setEmailError('Email is required'); 
+                errorCount++;               
             }
 
             // validate password was entered
             if (!password || password === '') {
-                setPasswordError('Password is required');                
+                setPasswordError('Password is required');   
+                errorCount++;             
             }
             
-            if(emailError || passwordError) {
+            if(errorCount > 0) {
                 throw new Error('Invalid email or password');
             }            
                       
@@ -74,20 +79,24 @@ export function Login(){
 
     return(
         <>
-        <h1>Login</h1>
+        <h1 className='display-1'>Login</h1>
         {loading && <p>Loading...</p>}
-        {error && <p>There was an error logging in</p>}
-        {success && <p>Login successful!</p>}
-        <form onSubmit={submit}>            
-            <label htmlFor="email">Email</label>
-            <input type="text" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)} />
-            {emailError && <p>{emailError}</p>}
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
-            {passwordError && <p>{passwordError}</p>}
-            <button type="submit" name="login" role="button">Login</button>
-            <button type="button" name="reset" role="button" onClick={reset}>Reset</button>
-        </form>
+        {error && <Alert variant="warning">There was an error logging in</Alert>}
+        {success && <Alert variant="success">Login successful!</Alert>}
+        <Form onSubmit={submit}>            
+            <Form.Group className='mb-3'>
+                <Form.Label htmlFor="email">Email</Form.Label>
+                <Form.Control type="text" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)} />
+                {emailError && <Alert variant='danger'>{emailError}</Alert>}
+            </Form.Group>
+            <Form.Group className='mb-3'>
+                <Form.Label htmlFor="password">Password</Form.Label>
+                <Form.Control type="password" id="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
+                {passwordError && <Alert variant='danger'>{passwordError}</Alert>}
+            </Form.Group>
+            <Button variant="primary" type="submit" name="login" role="button">Login</Button>
+            <Button variant="secondary" type="button" name="reset" role="button" onClick={reset}>Reset</Button>
+        </Form>
         </>
     );
 }
