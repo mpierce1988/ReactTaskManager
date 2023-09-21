f1d9d2ae0e2ef3834ef7b7755c8a679872c74672
michaelpierce <mpierce1138@gmail.com>
2023-09-21 18:50:27 -0300

    Changed name on Home page

--- task-manager/src/containers/Home.js
+++ task-manager/src/containers/Home.js
@@ -6,7 +6,7 @@ export function Home() {
         <Row className='justify-content-center'>
             <Col xs={12} md={8}>
             <h1 className='display-1'>Welcome to Task Manager!</h1>
-            <p className='lead'>Welcome to [Your App's Name], a React-based task management application designed to showcase my skills in web development.</p>
+            <p className='lead'>Welcome to React Task Manager, a React-based task management application designed to showcase my skills in web development.</p>
             <p> As a product of test-driven design, this application will help you manage your tasks through intuitive Create, Read, Update, and Delete (CRUD) functionalities. You can register as a new user or log in to unlock a tailored task management experience. Get started today and see how React can power robust and dynamic user interfaces!</p>
             </Col>
         </Row>
