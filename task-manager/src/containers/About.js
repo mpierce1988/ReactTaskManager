d5331e77f87dac92cb28b8184719b0e093279b9f
michaelpierce <mpierce1138@gmail.com>
2023-09-21 14:19:50 -0300

    Added bootstrap styling to About.js

--- task-manager/src/containers/About.js
+++ task-manager/src/containers/About.js
@@ -1,12 +1,13 @@
+import { Row, Col } from "react-bootstrap";
 
 export function About(){
     const currentYear = new Date().getFullYear();
     return (
-        <>
-            
-            <h1>Task Manager &copy; {currentYear}</h1>
+        <Row className='justify-content-center'>
+            <Col xs={12} md={8}>
+            <h1 className="display-1">Task Manager &copy; {currentYear}</h1>
             <p>Task Manager is a simple task management application built using React and Node.js.</p>
-            
-        </>
+            </Col>
+        </Row>
     );
 }
