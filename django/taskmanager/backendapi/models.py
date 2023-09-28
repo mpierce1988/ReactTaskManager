97c6250cdeb7c6159cbb8a1868f3a58c1f841aa8
michaelpierce <mpierce1138@gmail.com>
2023-09-28 12:09:57 -0300

    Updated User and Task model to use UUID

--- django/taskmanager/backendapi/models.py
+++ django/taskmanager/backendapi/models.py
@@ -1,15 +1,16 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
 
 class Task(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField()
