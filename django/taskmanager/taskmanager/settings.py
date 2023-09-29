b890b60da38336626673afe150de512a3a94e956
michaelpierce <mpierce1138@gmail.com>
2023-09-29 14:36:07 -0300

    Removed debug print statement from settings.py

--- django/taskmanager/taskmanager/settings.py
+++ django/taskmanager/taskmanager/settings.py
@@ -28,8 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-# TODO REMOVE DEBUG
-print("DEBUG value from config: ", config('DEBUG'))
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
